ba5f741f13da2f49a7818b2e73715c026f7c20ce
Liutx <15666882775@163.com>
2020-12-26 23:32:51 +0800

    门面模式传统调用

--- src/com/company/facade/ClientDemo.java
+++ src/com/company/facade/ClientDemo.java
@@ -11,5 +11,16 @@ public class ClientDemo {
     public static void main(String[] args) {
         VideoConversionFacade converter = new VideoConversionFacade();
         File mp4Video = converter.convertVideo("video.ogg", "mp4");
+
+
+        System.out.println("===================================");
+
+
+        VideoFile file = new VideoFile("TEST");
+        CodecFactory.extract(file);
+        BitrateReader.read(file,new OggCompressionCodec());
+        BitrateReader.convert(file,new MPEG4CompressionCodec());
+        AudioMixer audioMixer = new AudioMixer();
+        audioMixer.fix(file);
     }
 }
